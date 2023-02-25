8ddddff71c4c86673f1c9e1eff23d8bbb6f018b0
yes-corpo <125750075+yes-corpo@users.noreply.github.com>
2023-02-25 23:58:23 +0100

    Feature/controller (#4)
    * fix: tests deprecations
    
    * refacto: remove useless line
    
    * feat: simple json controller with test
    
    * fix: last "/" in uri
    
    * feat: controller with template

---
+++ src/Controller/SampleController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace YesCorpo\Bundle\SampleBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/sample', name: 'sample_')]
+class SampleController extends AbstractController
+{
+    #[Route('', name: 'index')]
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/SampleController.php',
+        ]);
+    }
+
+    #[Route('/detail', name: 'detail')]
+    public function detail()
+    {
+        return $this->render('@Sample/sample/detail.html.twig');
+    }
+}
---
+++ tests/Controller/SampleControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace YesCorpo\Bundle\SampleBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SampleControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/sample');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($client->getResponse()->getContent());
+        
+        $json = json_decode($client->getResponse()->getContent(), true);
+        $this->assertArrayHasKey('message', $json);
+        $this->assertArrayHasKey('path', $json);
+        $this->assertEquals('Welcome to your new controller!', $json['message']);
+        $this->assertEquals('src/Controller/SampleController.php', $json['path']);
+    }
+
+    public function testDetail()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/sample/detail');
+
+        $this->assertResponseIsSuccessful();
+
+        $this->assertStringContainsString('<h1>Detail</h1>', $client->getResponse()->getContent());
+    }
+}
