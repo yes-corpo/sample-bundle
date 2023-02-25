fa8ac68a18f027405654d583690b4caaba96e035
yes-corpo <yes.corpo@gmail.com>
2023-02-25 23:36:45 +0100

    feat: simple json controller with test

---
+++ src/Controller/SampleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace YesCorpo\Bundle\SampleBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/sample', name: 'sample_')]
+class SampleController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/SampleController.php',
+        ]);
+    }
+}
---
+++ tests/Controller/SampleControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace YesCorpo\Bundle\SampleBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SampleControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/sample/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($client->getResponse()->getContent());
+        
+        $json = json_decode($client->getResponse()->getContent(), true);
+        $this->assertArrayHasKey('message', $json);
+        $this->assertArrayHasKey('path', $json);
+        $this->assertEquals('Welcome to your new controller!', $json['message']);
+        $this->assertEquals('src/Controller/SampleController.php', $json['path']);
+    }
+}
